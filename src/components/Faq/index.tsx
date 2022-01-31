df4cea19ecc4b37311843cc3be34c9ed3e17de76
Felipe Moura <felipe.moura@interprocess.com.br>
2022-01-31 17:30:23 -0300

    refactor: separado faq do header deixando em um componente separado

---
+++ src/components/Faq/index.tsx
@@ -0,0 +1,14 @@
+import * as Styled from "./styles";
+export function Faq() {
+  return (
+    <Styled.Container>
+      <h1>Dúvidas</h1>
+      <p>
+        <strong>Listamos aqui algumas dúvidas frequentes,</strong>
+        <br></br>
+        caso a sua dúvida não esteja aqui você pode entrar em contato{" "}
+        <a href="#">clicanco aqui</a>
+      </p>
+    </Styled.Container>
+  );
+}
