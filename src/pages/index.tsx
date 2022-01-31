e61a2280b93c73c6f288c0d49e7adf18f4f279a7
Felipe Moura <felipe.moura@interprocess.com.br>
2022-01-31 14:51:00 -0300

    feat: realiza chamda para buscar informaçoes da api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,48 @@
-import type { NextPage } from "next";
+import axios from "axios";
+import type { GetStaticProps, NextPage } from "next";
+import { useEffect, useState } from "react";
+import { Questions } from "../components/Questions";
 
-const Home: NextPage = () => {
+type List = {
+  id: number;
+  titulo: string;
+  descricao: string;
+};
+type HomeData = {
+  data: List[];
+};
+
+const Home = ({ data }: HomeData) => {
+  const [questions, setQuestions] = useState<List[]>([]);
+  console.log(data);
+
+  useEffect(() => {
+    setQuestions(data);
+  }, [data]);
   return (
     <>
-      <h1>Hellow world</h1>
+      {questions &&
+        questions.map((question) => (
+          <Questions
+            key={question.id}
+            title={question.titulo}
+            description={question.descricao}
+          />
+        ))}
     </>
   );
 };
 
 export default Home;
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await axios.get("http://localhost:3000/api");
+  console.log(data);
+
+  return {
+    props: {
+      data,
+    },
+    revalidate: 60 * 60 * 24,
+  };
+};
